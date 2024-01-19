26b4f6814f01196d05bfacf68b6dfe3772c8832f
fadyshenoda0990 <fady.shenouda@com.svu.edu.eg.com>
2024-01-20 00:17:35 +0200

    last commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
     try {
       const response = await axiosInstance.get('/games');
       setList(response.data.results);
-      console.log('response', response.data.results);
+      // console.log('response', response.data.results);
       setLoading(false);
     } catch (error) {
       console.error('Error fetching games:', error);
